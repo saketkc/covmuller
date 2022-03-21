1adfe8ebb48b43da8471957bf5d986a739462d36
Saket Choudhary <saketkc@gmail.com>
2022-03-21 01:34:33 -0400

    Update docs

--- R/model.R
+++ R/model.R
@@ -1,9 +1,15 @@
+#' @importFrom magrittr %>%
+#' @importFrom tibble deframe
 #' @importFrom nnet multinom
-FitMultinom <- function(data, formula = as.formula("lineage_collapsed ~ MonthYearNumeric")) {
+FitMultinom <- function(data,
+                        formula = as.formula("lineage_collapsed ~ DateCollectedNumeric + State"),
+                        weights = "total_sequences") {
+  wt <- data %>% pull(!!weights) #%>% deframe()
+  print(wt)
   fit <- multinom(
     formula = formula,
     data = data,
-    weights = total_sequences,
+    weights = wt,
     maxit = 1000
   )
   return(fit)
