cfcbe4b342e2783a2c9a3b0273a63c17997b6d8d
Saket Choudhary <saketkc@gmail.com>
2022-09-24 20:20:51 -0400

    Data update

--- R/model.R
+++ R/model.R
@@ -5,7 +5,6 @@ FitMultinom <- function(data,
                         formula = as.formula("lineage_collapsed ~ DateCollectedNumeric + State"),
                         weights = "total_sequences") {
   wt <- data %>% pull(!!weights) # %>% deframe()
-  print(wt)
   fit <- multinom(
     formula = formula,
     data = data,
